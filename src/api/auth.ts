04b61ef3703252f4a844d61983e61a1c4b4bc4d8
Alexandor969 <sasapetrov01@gmail.com>
2023-10-17 19:58:03 +0300

    [fix] auth

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
 
-const URL = 'https://test.gnzs.ru/oauth/get-token.php'
 
 
 export default async function authorization() {
     try {
-        const response = await axios.get( URL, {
+        const response = await axios.get( 'https://test.gnzs.ru/oauth/get-token.php', {
             headers: {
                 'Content-Type': 'application/json',
                 "X-Client-Id": '31334466'
