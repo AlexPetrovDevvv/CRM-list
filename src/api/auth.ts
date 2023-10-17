59333af3399963767048bb2a83cab908d3ba4c7f
Alexandor969 <sasapetrov01@gmail.com>
2023-10-17 19:59:07 +0300

    [fix] auth

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -4,12 +4,7 @@ import axios from "axios";
 
 export default async function authorization() {
     try {
-        const response = await axios.get( 'https://test.gnzs.ru/oauth/get-token.php', {
-            headers: {
-                'Content-Type': 'application/json',
-                "X-Client-Id": '31334466'
-            }
-        })
+        const response = await axios.get( 'https://test.gnzs.ru/')
       console.log(response);
     } catch (error) {
       console.error(error);
